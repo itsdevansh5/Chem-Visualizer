19f87c8503d22eb4612b7494797010f80e79a6eb
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-11-23 01:57:04 +0530

    debugging api error

--- web/src/config.js
+++ web/src/config.js
@@ -1,5 +1,5 @@
 export const API_URL = import.meta.env.PROD
-  ? "https://chemvis-deploy.onrender.com/api"
+  ? "https://chemvis-deploy.onrender.com"
   : "http://127.0.0.1:8000/api/";
 
 export const TOKEN = "bd382495e92fd429d7e283a3e8f9bbe3b2398c3c";
